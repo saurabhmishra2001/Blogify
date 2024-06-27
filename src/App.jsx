28c8753c87622fbfbe33669045ac066dcc1321a5
Saurabh kumar Mishra <saurabhmishra31234@gmail.com>
2024-06-27 11:56:48 +0530

    add  store and authslice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth/auth'
+import { login,logout } from './store/authSlice' 
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+// import { Outlet } from 'react-router-dom'
 
 function App() {
 
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  const [loading,setLoading]=useState(true)
 
-  return (
-    <>
-     <h1>Blogify witgh appwrite</h1>
-    </>
+  const dispatch =useDispatch()
+
+  useEffect(()=>{
+    authService.getCutrrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[dispatch])
+  return !loading ? (
+    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+          <main>
+         TODO: {/* TODO:<Outlet/> */}
+          </main>
+          <Footer/>
+     
+      </div>
+    </div>
+  ) : (
+    <div className='min-h-sc'>Test not loading</div>
   )
 }
 
